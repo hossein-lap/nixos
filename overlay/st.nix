981a12233c10ba70836738d301f6943b0fea3786
hossein <hosteam01@gmail.com>
2024-06-10 22:22:25 +0330

    finish my build of st as an overlay [just enjoying]

--- overlay/st.nix
+++ overlay/st.nix
@@ -4,6 +4,7 @@
     version = "1.0.0";
     src = fetchTarball {
         url = "https://gitlab.com/hos-workflow/st/-/archive/master/st-master.tar.gz";
+        sha256 = "1598gn1lqinmwsh83fxdxaanrhx1q27d2l4sb09xwpiwxdp1grp5";
     };
     buildInputs = oldAttrs.buildInputs ++ (with super; [ harfbuzz ]);
   });
